8b7b4cdc557edf66c40e13159a297c9d907b5503
Kayla <k.kotewall@gmail.com>
2018-02-26 17:38:02 -0600

    update schema table

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -20,25 +20,18 @@ CREATE TABLE products (
 
 -- Creates new rows containing data in all named columns --
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Sleeping Bag", "Camping", 100, 2500);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Canteen", "Camping", 20, 450);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Insect Repellent", "Camping", 15, 5000);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Rawhide", "Pets", 8, 3200);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Furminator", "Pets", 45, 1500);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Cat Tree", "Pets", 100, 170);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Dry Kibble", "Pets", 35, 300);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Catnip Dispenser", "Pets", 30, 400);
+-- camping dept --
+VALUES ("Sleeping Bag", "Camping", 100, 2500),
+VALUES ("Canteen", "Camping", 20, 450),
+VALUES ("Insect Repellent", "Camping", 15, 5000),
+-- pet dept --
+VALUES ("Rawhide", "Pets", 8, 3200),
+VALUES ("Furminator", "Pets", 45, 1500),
+VALUES ("Cat Tree", "Pets", 100, 170),
+VALUES ("Dry Kibble", "Pets", 35, 300),
+VALUES ("Catnip Dispenser", "Pets", 30, 400),
+-- auto dept --
+VALUES ("Garage Floor Mat", "Auto", 65, 330),
+VALUES ("Tire Pressure Gauge", "Auto", 12, 4000),
+VALUES ("Floor Mats", "Auto", 55, 780),
+VALUES ("Auto Glass Cleaner", "Auto", 15, 650);
